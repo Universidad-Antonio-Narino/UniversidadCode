dd87f974d1dabf45ce1af7f89f73cb0cf4ab8034
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-03-15 22:53:38 -0500

    ya

--- CodeExercises/clasePila.cpp
+++ CodeExercises/clasePila.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
-#define MaxTamaPila 3
+#define MaxTamaPila 5
 class Pila{
 	private:
 		float pila[MaxTamaPila];
@@ -60,7 +60,7 @@ void Pila::imprimirPila(){
 
 void Pila::insertarP(float _elemento){
 	if (estallenaP()){
-		cout <<"!!! TENEMOS DESBORDAMIENTO EN LA PILAA!!!! eliminando cima eliminarla cima \n",Sleep(5000);
+		cout <<"!!! TENEMOS DESBORDAMIENTO EN LA PILAA!!!! eliminando cima eliminarla cima \n, Maximo de almacenamiento de la pila lleno",Sleep(5000);
 		quitarP( );
 		cin.get();
 		system("cls");
