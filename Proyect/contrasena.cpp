7607de1324b9573af5197b3adc7e4f79c89962b0
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-04-15 20:23:38 -0500

    new things

--- Proyect/contrasena.cpp
+++ Proyect/contrasena.cpp
@@ -9,14 +9,8 @@ using namespace std;
 class Contrasena
 {
 private:
-<<<<<<< HEAD
-   // string contrasenaa;
-    char letras[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    //string letras[26]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y", "z"};
-=======
    // Char contrasenia;
     char letras[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
 public:
     char generarmayus();
     char generarminus();
@@ -28,11 +22,7 @@ public:
 
 Contrasena::Contrasena()
 {
-<<<<<<< HEAD
-//contrasenaa="abcdefghij";
-=======
     //contrasenaa="abcdefghij";
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
 }
 
 Contrasena::~Contrasena()
@@ -42,13 +32,9 @@ Contrasena::~Contrasena()
 
 char Contrasena::generarmayus(){
     char may;
-<<<<<<< HEAD
-    may=letras[rand() % 26];
-=======
     //devuelve un caracter aletorio de la lista de letras
     may=letras[rand() % 26];
     // retorno de caracter en mayuscula
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
     return toupper(may);
 }
 
@@ -70,81 +56,71 @@ char Contrasena::generarcarac(){
 
 
 int main(){
-<<<<<<< HEAD
-    //string pasword="abcdefghij";
-    string pasword[10];
-    int l, m, s;
-    srand(time(0));
-    Contrasena p1;
-   // cout<<p1.generarmayus()<<p1.generarminus()<<p1.generarnum()<<p1.generarcarac();
-
-=======
-    //string pasword="abcdefghij" (caso prueba);
-    string pasword[10];
-    int l, m, s;
-    //generador de numeros aletorios
-    srand(time(0));
-    Contrasena p1;
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
-    //10 caracteres  == 3 ya ocupados(numero, mayuscula y especial) quedan 7 
-    //  7=100%
-    //normal =50%     mayuscula =15%
-    //numero =20%     especial 15%
-    for(int y=0; y<4; y++){
-<<<<<<< HEAD
-=======
-        /*genera un numero aletorio en el rango de 10,
-        se utilizara para guardar en un espacio aletorio cualquiera de los elementos
-        que componen la password 
-        */ 
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
-        s = rand() % 10;
-        if(pasword[s] == "" && y==0)
-            pasword[s]=p1.generarminus();
-        if(pasword[s] == "" && y==1)  
-            pasword[s]=p1.generarmayus();
-        if(pasword[s] == "" && y==2)
-            pasword[s]=p1.generarcarac();
-        if(pasword[s] == "" && y==3) 
-            pasword[s]=p1.generarnum();
-    }
-    for(int x=5; x<=10; x++){
-        inicio:
-        l=rand() % 10; //ubicar posicion del caracter
-        m=rand() % 101;
-        if(pasword[l] == ""){  
-            if(m<50)
-                pasword[l]=p1.generarminus();
-            else if(m<66 && m>49)
-                pasword[l]=p1.generarmayus();
-            else if(m<81 && m>65)
-                pasword[l]=p1.generarcarac();
-            else 
-                pasword[l]=p1.generarnum();
-        }
-        else{
-            goto inicio;
-        }
-    }
-<<<<<<< HEAD
-    cout<<"your pasword asigned is: ";
-=======
-    cout<<"Su contraseña asignada es: ";
->>>>>>> b404d4fcb082356e4f6b1d64629db85b586df19f
-    for(int x=0; x<10; x++)
-        cout<<pasword[x];
-    
-    //fichero
-  //arreglar lo de que sobreescribe
-    ofstream fich("acces.txt");
-    if (!fich)
+    int numDigits;
+    do
     {
-        cout << "Error al abrir ejemplo.dat\n";
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < 10; ++i)
-        fich << pasword[i];
+        cout<<"De Cuantos caracteres desea que se haga su contrasenia: ";
+        cin>>numDigits;
+        //string pasword="abcdefghij" (caso prueba);
+        string pasword[numDigits];
+        int l, m, s;
+        //generador de numeros aletorios
+        srand(time(0));
+        Contrasena p1;
+        //10 caracteres  == 3 ya ocupados(numero, mayuscula y especial) quedan 7 
+        //  7=100%
+        //normal =50%     mayuscula =15%
+        //numero =20%     especial 15%
+        for(int y=0; y<4; y++){
+            /*genera un numero aletorio en el rango de 10,
+            se utilizara para guardar en un espacio aletorio cualquiera de los elementos
+            que componen la password 
+            */ 
+            s = rand() % numDigits;
+            if(pasword[s] == "" && y==0)
+                pasword[s]=p1.generarminus();
+            if(pasword[s] == "" && y==1)  
+                pasword[s]=p1.generarmayus();
+            if(pasword[s] == "" && y==2)
+                pasword[s]=p1.generarcarac();
+            if(pasword[s] == "" && y==3) 
+                pasword[s]=p1.generarnum();
+        }
+        for(int x=5; x<=numDigits; x++){
+            inicio:
+            l=rand() % numDigits; //ubicar posicion del caracter
+            m=rand() % 101;
+            if(pasword[l] == ""){  
+                if(m<50)
+                    pasword[l]=p1.generarminus();
+                else if(m<66 && m>49)
+                    pasword[l]=p1.generarmayus();
+                else if(m<81 && m>65)
+                    pasword[l]=p1.generarcarac();
+                else 
+                    pasword[l]=p1.generarnum();
+            }
+            else{
+                goto inicio;
+            }
+        }
+        cout<<"Su contraseña asignada es: ";
+        for(int x=0; x<numDigits; x++)
+            cout<<pasword[x];
+        
+        //fichero
+        //arreglar lo de que sobreescribe
+        ofstream fich("acces.txt");
+        if (!fich)
+        {
+            cout << "Error al abrir ejemplo.dat\n";
+            exit(EXIT_FAILURE);
+        }
+        for (int i = 0; i < numDigits; ++i)
+            fich << pasword[i];
 
-    cout<<endl<<"su contrasena ya se encuentra registrada en nuestro sistema\n BIENVENIDO";
+        cout<<endl<<"su contrasena ya se encuentra registrada en nuestro sistema\n BIENVENIDO";    
+    } while (numDigits!=0 && numDigits!=25);
+    
 
 }
