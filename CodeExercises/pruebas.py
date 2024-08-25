a83f7ec2046daf666e50c26a36521bbe288092de
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-08-25 17:07:36 -0500

    Tarea lenguajes formales

--- CodeExercises/pruebas.py
+++ CodeExercises/pruebas.py
@@ -1,14 +1,4 @@
-cadena ="hello"
-sigma_estrella = list()
-def creator_substring(cadena):
-    if 1 == len(cadena) or len(cadena) == 0:
-        if len(cadena) == 1:
-            sigma_estrella.append(cadena)
-        return 
-    else:
-        for i in range(len(cadena),0,-1):
-            sigma_estrella.append(cadena[:i])
-        cadena=cadena[1:len(cadena)]
-        creator_substring(cadena)
-print(creator_substring(cadena))
-print(sigma_estrella)
+cadena = input()
+
+for less_ca in range(0,len(cadena)):
+    print(cadena[less_ca:])
