2b2e06792228ec16ef51527eefd4e3fb78653399
Santiago Angel Mancera <77251122+santiagoangel04@users.noreply.github.com>
2024-08-30 19:27:32 -0500

    Update teamSoccer.java

--- CodeExercises/teamSoccer.java
+++ CodeExercises/teamSoccer.java
@@ -2,7 +2,39 @@ import java.util.Random;
 public class Main
 {
 	public static void main(String[] args) {
+	    int SumaEquipoOne = 0;
+	    int SumaEquipoTwo = 0;
+		int[] Partido =new int[10];
 		Random rand = new Random();
-		S
+		for(int j = 0;j<10;j++){
+		  int team = rand.nextInt(2);
+		  if(j%2==0){
+		    Partido[j] = team;
+		  }
+		  else{
+		      Partido[j] = team;
+		  }
+		}
+		
+		for(int j = 0;j<10;j++){
+		  int team = rand.nextInt(2);
+		  if(j%2 == 0){
+		    SumaEquipoOne+=Partido[j];
+		  }
+		  else{
+		    SumaEquipoTwo+=Partido[j];
+		  }
+		}
+		System.out.println("Equipo 1: "+SumaEquipoOne);
+		System.out.println("Equipo 2: "+SumaEquipoTwo);
+		if(SumaEquipoTwo == SumaEquipoOne){
+		    System.out.println("Empate");
+		}else{
+		    if(SumaEquipoOne>SumaEquipoTwo){
+		        System.out.print("Gana el 1");
+		    }else{
+		        System.out.println("Gana el 2");
+		    }
+		}
 	}
 }
