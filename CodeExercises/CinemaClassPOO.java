d3194d55916c1b08b70759f949cadf70302ff0a6
Santiago Angel Mancera <77251122+santiagoangel04@users.noreply.github.com>
2024-08-30 20:26:01 -0500

    Create CinemaClassPOO.java (#26)

---
+++ CodeExercises/CinemaClassPOO.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Cinema{
+   public static void main(Strings[] args){
+        boolean[][] filas = new boolean[4][6];
+        Scanner input = new Scanner(System.in);
+        int fila = input.nextInt();
+        System.out.println("Ingrese la silla: ");
+        int silla = input.nextInt();
+        
+        filas[fila][silla] = true;
+        
+        for(int i = 0; i < filas.length;i++){
+          for(int j = 0; j<filas[])
+        }
+  }
+}
