1c28717922584f2325f3d1ef25dd6fc32a1c168f
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-08-31 21:51:30 -0500

    Cinema update

--- CodeExercises/CinemaClassPOO.java
+++ CodeExercises/CinemaClassPOO.java
@@ -1,45 +1,38 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.*;
-public class Main
-{
-    public static void main(String[] args){
-        boolean[][] filas = new boolean[4][6];
-        Scanner input = new Scanner(System.in);
-        int aforo = 0;
-        while (aforo != 24){
-            System.out.println("Ingrese la fila");
-            int fila = input.nextInt();
-            System.out.println("Ingrese la silla: ");
-            int silla = input.nextInt();
-        
-             System.out.println(aforo);
-            if(filas[fila-1][silla-1]){
-                System.out.println("Ocupado");
-            }
-            else{
-                filas[fila-1][silla-1] = true;
-            }
-            //Pintar cinema
-            for(int filaSearch = 0; filaSearch < filas.length;filaSearch++){
-                for(int sillaSearch = 0;sillaSearch<filas[0].length;sillaSearch++){
-                String ocupado = "";
-                if(filas[filaSearch][sillaSearch]){
-                    ocupado = "\t|x|\t";
-                    aforo+=1;
-                }else{
-                    ocupado = "\t|_|\t";
+
+public class CinemaClassPOO {
+        public static void main(String[] args){
+            boolean[][] filas = new boolean[4][6];
+            Scanner input = new Scanner(System.in);
+            int aforo = 0;
+            while (aforo != 24){
+                System.out.println("Ingrese la fila");
+                int fila = input.nextInt();
+                System.out.println("Ingrese la silla: ");
+                int silla = input.nextInt();
+            
+                if(filas[fila-1][silla-1]){
+                    System.out.println("Ocupado");
                 }
-                System.out.print(ocupado);
+                else{
+                    filas[fila-1][silla-1] = true;
+                    aforo+=1;
+                }
+                //Pintar cinema
+                for(int filaSearch = 0; filaSearch < filas.length;filaSearch++){
+                    for(int sillaSearch = 0;sillaSearch<filas[0].length;sillaSearch++){
+                    String ocupado = "";
+                    if(filas[filaSearch][sillaSearch]){
+                        ocupado = "\t|x|\t";
+                    }else{
+                        ocupado = "\t|_|\t";
+                    }
+                    System.out.print(ocupado);
+                    }
+                System.out.println();
+                }
+            System.out.println("\n\n\n\t\t\t|-------------------------------------------------|");
             }
-            System.out.println();
-            }    
+            System.out.println("Todos los asientos estan vendidas, ya no hay boletas para esta sala");
         }
-        
-    }
 }
---
+++ CodeExercises/Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        boolean[][] filas = new boolean[4][6];
+        Scanner input = new Scanner(System.in);
+        int aforo = 0;
+        while (aforo != 24){
+            System.out.println("Ingrese la fila");
+            int fila = input.nextInt();
+            System.out.println("Ingrese la silla: ");
+            int silla = input.nextInt();
+        
+             System.out.println(aforo);
+            if(filas[fila-1][silla-1]){
+                System.out.println("Ocupado");
+            }
+            else{
+                filas[fila-1][silla-1] = true;
+            }
+            //Pintar cinema
+            for(int filaSearch = 0; filaSearch < filas.length;filaSearch++){
+                for(int sillaSearch = 0;sillaSearch<filas[0].length;sillaSearch++){
+                String ocupado = "";
+                if(filas[filaSearch][sillaSearch]){
+                    ocupado = "\t|x|\t";
+                    aforo+=1;
+                }else{
+                    ocupado = "\t|_|\t";
+                }
+                System.out.print(ocupado);
+            }
+            System.out.println();
+            }    
+        }
+        
+    }
+}
