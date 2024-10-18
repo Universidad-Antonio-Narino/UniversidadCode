7a10162cd5b72d883b1fc6cc3565bb3e77285569
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-10-17 23:28:04 -0500

    tarea poo

--- Codigo java/clasepoo/src/main/java/com/viajaRapido/Ventas.java
+++ Codigo java/clasepoo/src/main/java/com/viajaRapido/Ventas.java
@@ -1,5 +1,40 @@
 package com.viajaRapido;
 
 public class Ventas {
+    private Empresa empresa;
+    private int numTickets;
+    private Ticket ticket;
+    private int total;
 
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public int getNumTickets() {
+        return numTickets;
+    }
+
+    public void setNumTickets(int numTickets) {
+        this.numTickets = numTickets;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
